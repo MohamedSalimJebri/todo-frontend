4f2e06554189a52cf35e57e1061a50c4e3bd99ab
MohamedSalimJebri <mohamedsalimjebri@gmail.com>
2026-03-31 13:42:53 +0100

    update API url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API = 'http://localhost:8000/api/todos/'
+const API = 'https://todo-backend-4cpt.onrender.com/api/todos/'
 
 export default function App() {
   const [todos, setTodos] = useState([])
